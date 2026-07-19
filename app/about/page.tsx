8f2afd2756853c18af3bf024d1396e5dd979da16
SajithNuski <sajithnuski555@gmail.com>
2026-07-20 01:52:16 +0530

    test

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <div className="flex flex-col items-center h-screen justify-center gap-4">
+          <h1 className="text-3xl font-bold text-emerald-600">Hello this is a test</h1>
+          <h1 className="text-3xl font-bold text-amber-800">Hellooooo</h1>
+        </div>
+    </div>
+  )
+}
+
+export default page
---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    
+        <div className="flex flex-col items-center h-screen justify-center gap-4">
+          <h1 className="text-3xl font-bold text-emerald-300">Hello this is a test</h1>
+          <h1 className="text-3xl font-bold text-amber-300">Hellooooo</h1>
+        </div>
+  );
+}
